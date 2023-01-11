a4c0c3da11efb7a90ef434f97abe49a2bd5ceef2
Diego <diegochueri@gmail.com>
2023-01-11 16:26:19 -0400

    feat: added method to add seasons and episodes of a series

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,5 +17,12 @@ class Series extends Model
     public function seasons()
     {
         return $this->hasMany(Season::class);
+    }
+
+    protected static function booted()
+    {
+        self::addGlobalScope('ordered', function (Builder $queryBuilder) {
+            $queryBuilder->orderBy('name');
+        });
     }
 }
