d59a9591a4db7b15d2871f6dbbd1e7cc9ccac4dd
diego <diegochueri@gmail.com>
2023-01-12 12:23:12 -0400

    feat: added index of seasons method

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/series', SeriesController::class);
+
+Route::apiResource('/series/{series}/seasons', SeasonsController::class);
