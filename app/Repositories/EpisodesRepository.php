ec54118cf1bd7c4625236ae7bd512de56c3756c6
diego <diegochueri@gmail.com>
2023-01-13 22:50:26 -0400

    feat: add episodes repository

---
+++ app/Repositories/EpisodesRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Http\Request;
+
+interface EpisodesRepository
+{
+    public function getAllOfSeason(int $seasonId);
+    public function updateIfWasWatched(int $episodeId, Request $request): void;
+}
